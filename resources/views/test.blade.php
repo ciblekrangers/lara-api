d11c7fbc6dbff36d2fa08ee0846398606e66b06a
ciblekrangers <ciblekrangers@gmail.com>
2024-03-29 13:48:15 +0700

    v1

---
+++ resources/views/test.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="bg-red-400 p-32 hover:bg-black w-96">
+        test
+    </div>
+    <div class="grid grid-cols-10 gap-2">
+        <div class="bg-sky-50 aspect-square"></div>
+        <div class="bg-sky-100 aspect-square"></div>
+        <div class="bg-sky-200 aspect-square"></div>
+        <div class="bg-sky-300 aspect-square"></div>
+        <div class="bg-sky-400 aspect-square"></div>
+        <div class="bg-sky-500 aspect-square"></div>
+        <div class="bg-sky-600 aspect-square"></div>
+        <div class="bg-sky-700 aspect-square"></div>
+        <div class="bg-sky-800 aspect-square"></div>
+        <div class="bg-sky-900 aspect-square"></div>
+    </div>
+</body>
+
+</html>
